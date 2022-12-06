d872414f0b2ba1912a91b5a2d9263f952e545ef1
alexvplle <alexisvapaille11@gmail.com>
2022-12-07 00:48:50 +0100

    add countries

--- pages/api/createCheckout.ts
+++ pages/api/createCheckout.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	const session = await stripe.checkout.sessions.create({
 		mode: 'payment',
 		allow_promotion_codes : true,
-		shipping_address_collection: {allowed_countries: ['FR']},
+		shipping_address_collection: {allowed_countries: ['FR', 'DE', 'AR', 'BE', 'BR', 'CA', 'CN', 'KR', 'ES', 'GR', 'GF', 'IT', 'GB', 'US']},
 		line_items : lineItems,
 		success_url: `https://www.elpe-clothing.com/sucess?session_id={CHECKOUT_SESSION_ID}`,
 		cancel_url: 'https://www.elpe-clothing.com/'
