2e1bca6beadcc134472c1581f96e0ff1224d53e4
alexvplle <alexisvapaille11@gmail.com>
2022-12-07 00:31:58 +0100

    add coupons

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -9,7 +9,7 @@ export default function Checkout({ cart } : { cart : ContentInCart[] }) {
 	const router = useRouter()
 	const lineItems : Array<Item> = cart.map(function(item) { return { "price" : item.price_id, "quantity" : item.quantity } })
 	const redirectToCheckout = () => { 
-		fetch('http://localhost:3000/api/createCheckout',
+		fetch('https://www.elpe-clothing.com/api/createCheckout',
 		{
 			method: 'POST',
 			body: JSON.stringify({ lineItems })
