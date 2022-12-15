f539d8c947327a892398de5492ffbe1119e1e28f
alexvplle <alexisvapaille11@gmail.com>
2022-12-15 22:46:15 +0100

    little advancement

--- components/Mascot.tsx
+++ components/Mascot.tsx
@@ -1,7 +1,9 @@
 import * as THREE from "three";
 import React from "react";
-import { useGLTF, useTexture } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -9,8 +11,8 @@ type GLTFResult = GLTF & {
     Hand: THREE.Mesh;
     Ear: THREE.Mesh;
     Neck: THREE.Mesh;
-    Maillage_3: THREE.Mesh;
-    Maillage_4: THREE.Mesh;
+    Mesh003: THREE.Mesh;
+    Mesh003_1: THREE.Mesh;
     Retopo_Hoodie_Pocket_lo: THREE.Mesh;
     Retopo_Hoodie_Hood_lo: THREE.Mesh;
     Hoodie_Tassel1_lo: THREE.Mesh;
@@ -36,114 +38,145 @@ type GLTFResult = GLTF & {
     ["Shoe 1"]: THREE.MeshStandardMaterial;
     ["Shoe laces"]: THREE.MeshStandardMaterial;
     Shoe: THREE.MeshStandardMaterial;
+    ["Material.002"]: THREE.MeshStandardMaterial;
   };
 };
 
 export default function Model(props: JSX.IntrinsicElements["group"]) {
-  const { nodes, materials } = useGLTF("/ELPEMascot.gtlf") as unknown as GLTFResult;
-  const textureJean = useTexture("texture/textureJean.jpg")
-  const firstLogo = useTexture("texture/logo1.png")
-  const secondLogo = useTexture("texture/logo2.png")
-  const thirdLogo = useTexture("texture/logo3.png")
-  const fourthLogo = useTexture("texture/logo4.png")
+  const { nodes, materials } = useGLTF("/mascot/ELPEMascot.gltf") as unknown as GLTFResult;
+  const jeanTexture = useLoader(TextureLoader, '/mascot/texture/jeanTexture.jpg')
+  materials["Hoodie  - Secondary"].color = new THREE.Color(0x8638c6)
+  materials["Hoodie Pocket"].color = new THREE.Color(0x8638c6)
+  materials.Hoodie.color = new THREE.Color(0x8638c6)
+  materials["Material.002"].color = new THREE.Color(0x425d8c)
   return (
     <group {...props} dispose={null}>
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Ear_ring.geometry}
         material={materials["Material.001"]}
         position={[-0.68, 8.22, 0.11]}
         rotation={[-0.35, -0.71, -2.08]}
       />
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Hand.geometry}
         material={materials.Skin}
         position={[-1.75, 4.9, -0.16]}
         rotation={[0, -1.26, 0]}
       />
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Ear.geometry}
         material={materials.Skin}
         position={[-0.01, 8.4, 0.09]}
       />
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Neck.geometry}
         material={materials.Skin}
         position={[-0.01, 7.72, 0.09]}
       />
       <group position={[-0.01, 8.64, 0.33]} scale={5.14}>
         <mesh
-          geometry={nodes.Maillage_3.geometry}
+          castShadow
+          receiveShadow
+          geometry={nodes.Mesh003.geometry}
           material={materials["Skin.003"]}
         />
         <mesh
-          geometry={nodes.Maillage_4.geometry}
+          castShadow
+          receiveShadow
+          geometry={nodes.Mesh003_1.geometry}
           material={materials["Material.005"]}
         />
       </group>
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Retopo_Hoodie_Pocket_lo.geometry}
+        material={materials["Hoodie Pocket"]}
         position={[-0.02, 5.02, 1.08]}
         rotation={[Math.PI / 2, 0, 0]}
-      >
-	 </mesh>
+      />
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Retopo_Hoodie_Hood_lo.geometry}
+        material={materials["Hoodie  - Secondary"]}
         position={[0.04, 7.79, -0.26]}
         rotation={[Math.PI / 2, 0, 0]}
       />
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Hoodie_Tassel1_lo.geometry}
+        material={materials["Hoodie - Tassel"]}
         position={[0.02, 6.56, 0.93]}
         rotation={[Math.PI / 2, 0, 0]}
-      >
-	 </mesh>
+      />
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Circle.geometry}
+        material={materials["Hoodie Under shirt"]}
         position={[-0.01, 7.66, 0.07]}
         rotation={[0.07, 0, 0]}
-      >
-	 	<meshBasicMaterial map={firstLogo} />
-	 </mesh>
+      />
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Retopo_Hoodie_lo002.geometry}
+        material={materials.Hoodie}
         position={[0.01, 5.73, 0.21]}
         rotation={[Math.PI / 2, 0, 0]}
         scale={1.49}
-      >
-	 	<meshBasicMaterial map={firstLogo} />
-	 </mesh>
+      />
       <group
         position={[0.94, -0.06, 0.55]}
         rotation={[Math.PI / 2, 0, -0.38]}
         scale={0.47}
       >
         <mesh
+          castShadow
+          receiveShadow
           geometry={nodes.Tênis_Plane005_1.geometry}
           material={materials["Shoe 2"]}
         />
         <mesh
+          castShadow
+          receiveShadow
           geometry={nodes.Tênis_Plane005_2.geometry}
           material={materials["Shoe 1"]}
         />
         <mesh
+          castShadow
+          receiveShadow
           geometry={nodes.Tênis_Plane005_3.geometry}
           material={materials["Shoe laces"]}
         />
         <mesh
+          castShadow
+          receiveShadow
           geometry={nodes.Tênis_Plane005_4.geometry}
           material={materials.Shoe}
         />
       </group>
       <mesh
+        castShadow
+        receiveShadow
         geometry={nodes.Circle001.geometry}
-	   material={materials["Material.001"]}
+        material={materials["Material.002"]}
         position={[-0.02, 4.84, 0.23]}
         scale={5.19}
-      >
-	 	<meshBasicMaterial attach={"material"} color={"#FF0000"}/>
-	 </mesh>
+      />
     </group>
   );
 }
 
-useGLTF.preload("/ELPEMascot.gtlf");
+useGLTF.preload("/mascot/ELPEMascot.gltf");
+
--- pages/custom.tsx
+++ pages/custom.tsx
@@ -21,7 +21,7 @@ export default function Custom() {
 					<Canvas>
 						<Suspense fallback={<Loader />}>
 							<OrbitControls enablePan={false} minPolarAngle={-Math.PI / 2} maxPolarAngle={Math.PI / 2} minAzimuthAngle={- Math.PI / 2} maxAzimuthAngle={Math.PI / 2} maxDistance={8} minDistance={5} minZoom={10000} />
-							<Stage environment="city" intensity={1}>
+							<Stage environment="city" intensity={0.5}>
 								<Model />
 							</Stage>
 						</Suspense>
