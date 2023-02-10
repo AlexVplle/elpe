d7a2b27543e31041c5d1dddb13dabe92496ae96c
alexvplle <alexisvapaille11@gmail.com>
2023-02-10 23:52:46 +0100

    finish

--- pages/api/createCheckout.ts
+++ pages/api/createCheckout.ts
@@ -15,10 +15,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	// 	const { default_price } = productFound.data[0]
 	// 		return { price: default_price as string, quantity: quantity}
 	// }))
-	const session : Stripe.Response<Stripe.Checkout.Session> = await stripe.checkout.sessions.create({
+	const session = await stripe.checkout.sessions.create({
 		mode: 'payment',
-		allow_promotion_codes : true,
-		shipping_address_collection: {allowed_countries: ['FR', 'DE', 'AR', 'BE', 'BR', 'CA', 'CN', 'KR', 'ES', 'GR', 'GF', 'IT', 'GB', 'US']},
 		line_items : [{ price : 'price_1MZgzLJAtKEdmzDNDGTHC76l', quantity: 3 }],
 		success_url: `https://www.elpe-clothing.com/sucess?session_id={CHECKOUT_SESSION_ID}`,
 		cancel_url: 'https://www.elpe-clothing.com/'
