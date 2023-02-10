d91e64c7a76e16edc4a4d74f6fdd6c892ea16d8a
alexvplle <alexisvapaille11@gmail.com>
2023-02-10 22:01:01 +0100

    finish

--- pages/api/createCheckout.ts
+++ pages/api/createCheckout.ts
@@ -7,7 +7,6 @@ const stripe = new Stripe(stripeAPI, { apiVersion: '2022-08-01'})
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	// const { cart } : { cart : Array<contentInCart> }= JSON.parse(req.body)
-	// const lineItems = [{ price : 'price_1MZgzLJAtKEdmzDNDGTHC76l', quantity: 3 }]
 	// const lineItems : Array<{ price: string, quantity: number}> = await Promise.all(cart.map(async ({ name, price, quantity, taille }) => {
 	// 	const productFound = await stripe.products.search({
 	// 		query:`name~'${name} TAILLE ${taille}'`
@@ -15,14 +14,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	// 	const { default_price } = productFound.data[0]
 	// 		return { price: default_price as string, quantity: quantity}
 	// }))
-	// const session : Stripe.Response<Stripe.Checkout.Session> = await stripe.checkout.sessions.create({
-	// 	mode: 'payment',
-	// 	allow_promotion_codes : true,
-	// 	shipping_address_collection: {allowed_countries: ['FR', 'DE', 'AR', 'BE', 'BR', 'CA', 'CN', 'KR', 'ES', 'GR', 'GF', 'IT', 'GB', 'US']},
-	// 	line_items : lineItems,
-	// 	success_url: `https://www.elpe-clothing.com/sucess?session_id={CHECKOUT_SESSION_ID}`,
-	// 	cancel_url: 'https://www.elpe-clothing.com/'
-	// })
-	// res.status(200).json({ session })
-	res.status(200).json({ 'test': 'test'})
+	const session : Stripe.Response<Stripe.Checkout.Session> = await stripe.checkout.sessions.create({
+		mode: 'payment',
+		allow_promotion_codes : true,
+		shipping_address_collection: {allowed_countries: ['FR', 'DE', 'AR', 'BE', 'BR', 'CA', 'CN', 'KR', 'ES', 'GR', 'GF', 'IT', 'GB', 'US']},
+		line_items : [{ price : 'price_1MZgzLJAtKEdmzDNDGTHC76l', quantity: 3 }],
+		success_url: `https://www.elpe-clothing.com/sucess?session_id={CHECKOUT_SESSION_ID}`,
+		cancel_url: 'https://www.elpe-clothing.com/'
+	})
+	res.status(200).json({ session })
 }
