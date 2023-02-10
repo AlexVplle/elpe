0c2455cfb7a21f165eadcf6559403431f181a4c2
alexvplle <alexisvapaille11@gmail.com>
2023-02-10 19:52:44 +0100

    finish

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -10,7 +10,7 @@ export default function Checkout({ cart } : { cart : Array<ContentInCart> }) {
 	const router : NextRouter = useRouter()
 	const redirectToCheckout : MouseEventHandler<HTMLButtonElement> = () => { 
 		setClicked(true)
-		fetch(`https://www.elpe-clothing.com/api/createCheckout`,
+		fetch('https://www.elpe-clothing.com/api/createCheckout',
 		{
 			method: 'POST',
 			body: JSON.stringify({ cart })
