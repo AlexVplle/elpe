3d4a3173ef5438b37d84175937672e04973a4d77
alexvplle <alexisvapaille11@gmail.com>
2023-02-10 23:35:28 +0100

    finish

--- pages/api/createCheckout.ts
+++ pages/api/createCheckout.ts
@@ -8,13 +8,13 @@ const stripe = new Stripe(stripeAPI, { apiVersion: '2022-11-15'})
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const { cart } : { cart : Array<contentInCart> }= JSON.parse(req.body)
-	const lineItems : Array<{ price: string, quantity: number}> = await Promise.all(cart.map(async ({ name, price, quantity, taille }) => {
-		const productFound = await stripe.products.search({
-			query:`name~'${name} TAILLE ${taille}'`
-		})
-		const { default_price } = productFound.data[0]
-			return { price: default_price as string, quantity: quantity}
-	}))
+	// const lineItems : Array<{ price: string, quantity: number}> = await Promise.all(cart.map(async ({ name, price, quantity, taille }) => {
+	// 	const productFound = await stripe.products.search({
+	// 		query:`name~'${name} TAILLE ${taille}'`
+	// 	})
+	// 	const { default_price } = productFound.data[0]
+	// 		return { price: default_price as string, quantity: quantity}
+	// }))
 	// const session : Stripe.Response<Stripe.Checkout.Session> = await stripe.checkout.sessions.create({
 	// 	mode: 'payment',
 	// 	allow_promotion_codes : true,
@@ -23,5 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	// 	success_url: `https://www.elpe-clothing.com/sucess?session_id={CHECKOUT_SESSION_ID}`,
 	// 	cancel_url: 'https://www.elpe-clothing.com/'
 	// })
-	res.status(200).json({ 'test' : lineItems })
+	const response = await fetch('https://api.chucknorris.io/jokes/random');
+	const data = await response.json()
+	res.status(200).json({ 'test' : data })
 }
