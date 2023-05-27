cc0f921cc738f9c60db354ac15d91dbf9acc65b3
alexvplle <alexisvapaille11@gmail.com>
2023-05-27 15:27:07 +0200

    finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,13 @@ import Image from 'next/image'
 import HeadAPP from '../components/Head'
 
 import styles from '../styles/index.module.css'
+import { useEffect } from 'react'
 
 export default function Index() {
+	useEffect(() => {
+		const video : HTMLVideoElement = document.getElementById("video") as HTMLVideoElement
+		video.play()
+	}, [])
 	const router : NextRouter = useRouter()
 	const redirectHome = function() {
 		router.push('/home')
@@ -15,8 +20,7 @@ export default function Index() {
 	return (
 		<div className={styles.body}>
 			<HeadAPP />
-			<video autoPlay loop muted playsInline className={styles.video} src="clothes/elpeAccessories/home.mp4" >
-			</video>
+			<video autoPlay loop muted playsInline className={styles.video} id="video" src="clothes/elpeAccessories/home.mp4" />
 			<button className={styles.button} onClick={redirectHome}><div className={styles.textButton} id='text'>Entrez →</div><div className={styles.logo} id="logo"><Image src="/icons/logo.gif" width={70} height={70} alt={"test"}/></div></button>
 		</div>
 	)
